4b5a79d408e00795c5fc763d362d31c061913b70
Marco Torres <702700+daikucafe@users.noreply.github.com>
2023-10-23 18:01:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Import the necessary modules
+const express = require('express');
+
+// Create an instance of Express
+const app = express();
+const port = 3000; // You can choose any port you prefer
+
+// Define a route for handling GET requests to /comments
+app.get('/comments', (req, res) => {
+  res.send('This is the comments route!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
